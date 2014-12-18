69bde59bf2442174460f13c744529e70b054d389
stefanlasiewski <stefan@stefanco.com>
2014-12-18 14:59:03 -0800

    First commit.

---
+++ foreman-backup.sh
@@ -0,0 +1,39 @@
+#!/usr/bin/env bash
+
+# Backup The Foreman, following the advice at 
+# http://theforeman.org/manuals/1.7/index.html#5.5.1Backup
+
+# stefanl@nersc.gov
+
+# Temporary: Be verbose
+#set -x
+
+# If any command fails, stop this script.
+set -e
+
+ME=`basename $0`
+
+main () {
+
+	DATE=$(date '+%Y%m%d.%H%M')
+	BACKUPDIR=/data/backups/backup-$DATE
+	mkdir $BACKUPDIR
+	chgrp postgres $BACKUPDIR
+	chmod g+w $BACKUPDIR
+	
+	cd $BACKUPDIR
+	
+	# Backup postgres database
+	su - postgres -c "pg_dump -Fc foreman > $BACKUPDIR/foreman.dump"
+	
+	# Backup config ifles
+	
+	tar --selinux -czf $BACKUPDIR/etc_foreman_dir.tar.gz /etc/foreman
+	tar --selinux -czf $BACKUPDIR/var_lib_puppet_dir.tar.gz /var/lib/puppet/ssl
+	tar --selinux -czf $BACKUPDIR/tftpboot-dhcp.tar.gz /var/lib/tftpboot /etc/dhcp/ /var/lib/dhcpd/
+	
+	ls -lh *tar.gz
+
+}
+
+main 2>&1 | /usr/bin/logger -t $ME
